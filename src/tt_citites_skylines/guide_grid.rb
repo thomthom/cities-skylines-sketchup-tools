60a2be94ac5b86db6ab6ff37231548f6706f1f57
brotuser <brotuser@web.de>
2015-04-19 16:32:27 +0200

    Update guide_grid.rb
    Height Grid

--- src/tt_citites_skylines/guide_grid.rb
+++ src/tt_citites_skylines/guide_grid.rb
@@ -14,16 +14,25 @@ module TT::Plugins::CitiesSkylinesTools
   GRID_CELLS_X = "GridCellsX".freeze
   GRID_CELLS_Y = "GridCellsY".freeze
   GRID_CELLS_SUBDIVS = "GridCellsSubDivs".freeze
+
+  HEIGHT_GRID = "HeightGrid".freeze
+  FLOORS = "Floors".freeze
+  FLOOR_HEIGHT = "FloorHeight".freeze
   
-  def self.create_guide_grid(grid_x = nil, grid_y = nil, grid_subdivs = nil, group = nil)
+  
+  def self.create_guide_grid(grid_x = nil, grid_y = nil, grid_subdivs = nil, height_grid = nil, floors = nil, floor_height = nil, group = nil)
     
     # Prompt user for grid size/
-    prompts = ["Grid Width:", "Grid Depth:" , "Subdivisions:"]
+    prompts = ["Grid Width:", "Grid Depth:" , "Subdivisions:", "Height Grid:", "Floors:", "Floor Height:"]
     default_cell_x = Sketchup.read_default(PLUGIN_ID, GRID_CELLS_X, 4)
     default_cell_y = Sketchup.read_default(PLUGIN_ID, GRID_CELLS_Y, 4)
     default_cell_subdivs = Sketchup.read_default(PLUGIN_ID, GRID_CELLS_SUBDIVS, 1)
-    defaults = [default_cell_x, default_cell_y, default_cell_subdivs]
-    list = ["1|2|3|4|5|6|7|8", "1|2|3|4|5|6|7|8" , "0|1|2|3"]
+    default_height_grid = Sketchup.read_default(PLUGIN_ID, HEIGHT_GRID, "Yes")
+    default_floors = Sketchup.read_default(PLUGIN_ID, FLOORS, 5)
+    default_floor_height = Sketchup.read_default(PLUGIN_ID, FLOOR_HEIGHT, 2.9)
+
+    defaults = [default_cell_x, default_cell_y, default_cell_subdivs, default_height_grid, default_floors, default_floor_height]
+    list = ["1|2|3|4|5|6|7|8", "1|2|3|4|5|6|7|8" , "0|1|2|3", "No|Yes", "", ""]
     input = UI.inputbox(prompts, defaults, list, "Grid Dimensions")
     return false if input === false
     
@@ -31,6 +40,10 @@ module TT::Plugins::CitiesSkylinesTools
     cells_x = input[0]
     cells_y = input[1]
     cells_subdivs = input[2]
+    height_grid = input[3]
+    floors = input[4]
+    floor_height = input[5]
+    
     grid_lines_x = cells_x + 1
     grid_lines_y = cells_y + 1
     
@@ -38,6 +51,9 @@ module TT::Plugins::CitiesSkylinesTools
     Sketchup.write_default(PLUGIN_ID, GRID_CELLS_X, cells_x)
     Sketchup.write_default(PLUGIN_ID, GRID_CELLS_Y, cells_y)
     Sketchup.write_default(PLUGIN_ID, GRID_CELLS_SUBDIVS, cells_subdivs)
+    Sketchup.write_default(PLUGIN_ID, HEIGHT_GRID, height_grid)
+    Sketchup.write_default(PLUGIN_ID, FLOORS, floors)
+    Sketchup.write_default(PLUGIN_ID, FLOOR_HEIGHT, floor_height)
     
     model = Sketchup.active_model
     model.start_operation("Guide Grid")
@@ -48,21 +64,28 @@ module TT::Plugins::CitiesSkylinesTools
     group.set_attribute(PLUGIN_ID, GRID_CELLS_X, cells_x)
     group.set_attribute(PLUGIN_ID, GRID_CELLS_Y, cells_y)
     group.set_attribute(PLUGIN_ID, GRID_CELLS_SUBDIVS, cells_subdivs)
-
+    group.set_attribute(PLUGIN_ID, HEIGHT_GRID, height_grid)
+    group.set_attribute(PLUGIN_ID, FLOORS, floors)
+    group.set_attribute(PLUGIN_ID, FLOOR_HEIGHT, floor_height)
+    floors = 1 unless default_height_grid == "Yes"
+    floors = floors + 1 if default_height_grid == "Yes"
     # Add guide points.
-    grid_lines_x.times { |x|
-      grid_lines_y.times { |y|
-        group.entities.add_cpoint([x * SECTOR_SIZE, y * SECTOR_SIZE, 0])
+
+    floors.times { |i|
+      grid_lines_x.times { |x|
+        grid_lines_y.times { |y|
+          group.entities.add_cpoint([x * SECTOR_SIZE, y * SECTOR_SIZE, i * floor_height.m ])
+        }
       }
-    }
-    # Add guide lines.
-    grid_lines_x.times { |x|
-      grid_lines_y.times { |y|
-        point1 = Geom::Point3d.new(x * SECTOR_SIZE, y * SECTOR_SIZE, 0)
-        point2 = point1.offset(X_AXIS, SECTOR_SIZE)
-        point3 = point1.offset(Y_AXIS, SECTOR_SIZE)
-        group.entities.add_cline(point1, point2) unless x == cells_x
-        group.entities.add_cline(point1, point3) unless y == cells_y
+      # Add guide lines.
+      grid_lines_x.times { |x|
+        grid_lines_y.times { |y|
+          point1 = Geom::Point3d.new(x * SECTOR_SIZE, y * SECTOR_SIZE, i * floor_height.m )
+          point2 = point1.offset(X_AXIS, SECTOR_SIZE)
+          point3 = point1.offset(Y_AXIS, SECTOR_SIZE)
+          group.entities.add_cline(point1, point2) unless x == cells_x
+          group.entities.add_cline(point1, point3) unless y == cells_y
+        }
       }
     }
     
@@ -77,12 +100,13 @@ module TT::Plugins::CitiesSkylinesTools
     }
     cells_subdivs_x = cells_subdivs_x + 1
     cells_subdivs_y = cells_subdivs_y + 1
-    
     cells_subdivs_x.times { |x|
       cells_subdivs_y.times { |y|
-        group.entities.add_cpoint([x * cells_subdivs_step, y * cells_subdivs_step, 0])
+        group.entities.add_cpoint([x * cells_subdivs_step, y * cells_subdivs_step, 0 ])
       }
     }
+    
+    
     
     # Center at origin.
     half_size_x = (SECTOR_SIZE * cells_x) / 2.0
@@ -111,9 +135,12 @@ module TT::Plugins::CitiesSkylinesTools
     grid_x = entity.get_attribute(PLUGIN_ID, GRID_CELLS_X)
     grid_y = entity.get_attribute(PLUGIN_ID, GRID_CELLS_Y)
     grid_subdivs = entity.get_attribute(PLUGIN_ID, GRID_CELLS_SUBDIVS)
+    height_grid = entity.get_attribute(PLUGIN_ID, HEIGHT_GRID)
+    floors = entity.get_attribute(PLUGIN_ID, FLOORS)
+    floor_height = entity.get_attribute(PLUGIN_ID, FLOOR_HEIGHT)
     return false if grid_x.nil? || grid_y.nil? || grid_subdivs.nil?
     context_menu.add_item("Edit Grid") {
-      self.create_guide_grid(grid_x, grid_y, grid_subdivs, entity)
+      self.create_guide_grid(grid_x, grid_y, grid_subdivs, height_grid, floors, floor_height, entity)
     }
     true
   end
