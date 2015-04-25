e278e02d3b8a96335452311635140f7f6383c04b
brotuser <blackbox@Blacks-iMac.local>
2015-04-25 20:31:18 +0200

    Scale and Rotate

--- src/tt_citites_skylines/exporter_dae.rb
+++ src/tt_citites_skylines/exporter_dae.rb
@@ -78,7 +78,32 @@ module TT::Plugins::CitiesSkylinesTools
 
   def self.export_dae(target = nil)
     model = Sketchup.active_model
+    zeroPoint = Geom::Point3d.new
+    v1 = Geom::Vector3d.new 0,1,0
+    # Rotate and Scale
+    t_scale = Geom::Transformation.scaling(zeroPoint, 2.54) # 3D
+    v2 = Geom::Vector3d.new 0,0,-1
+    angle = v1.angle_between(v2)
+    ts = 1.0 / t_scale.to_a[15]
+    ta = t_scale.to_a.collect { |d| d * ts }
+    t_scale = Geom::Transformation.new(ta)
+    v3 = v1 * v2
+    t_rotation = Geom::Transformation.rotation(zeroPoint, v3, angle)
+    t = t_rotation * t_scale
+    model.active_entities.transform_entities(t, model.selection)
+    # Export
     model.export(target)
+    # Reset
+    t_scale = Geom::Transformation.scaling(zeroPoint, 1/2.54) # 3D
+    v2 = Geom::Vector3d.new 0,0,1
+    angle = v1.angle_between(v2)
+    ts = 1.0 / t_scale.to_a[15]
+    ta = t_scale.to_a.collect { |d| d * ts }
+    t_scale = Geom::Transformation.new(ta)
+    v3 = v1 * v2
+    t_rotation = Geom::Transformation.rotation(zeroPoint, v3, angle)
+    t = t_rotation * t_scale
+    model.active_entities.transform_entities(t, model.selection)
   end
 
 end # module TT::Plugins::CitiesSkylinesTools
